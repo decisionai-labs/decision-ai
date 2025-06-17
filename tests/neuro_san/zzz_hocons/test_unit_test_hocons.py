e9f3c5b94d7999d28efd27dd37b3756b7f69e83b
vincent.nguyen <vincent.nguyen@cognizant.com>
2025-06-16 20:42:35 -0700

    added pytest time

--- tests/neuro_san/zzz_hocons/test_unit_test_hocons.py
+++ tests/neuro_san/zzz_hocons/test_unit_test_hocons.py
@@ -9,6 +9,8 @@
 #
 # END COPYRIGHT
 
+import pytest
+
 from unittest import TestCase
 
 from parameterized import parameterized
@@ -38,6 +40,7 @@ class TestUnitTestHocons(TestCase):
 
         # List more hocon files as they become available here.
     ]))
+    @pytest.mark.timeout(10)  # 10 seconds for this test
     def test_hocon(self, test_name: str, test_hocon: str):
         """
         Test method for a single parameterized test case specified by a hocon file.
