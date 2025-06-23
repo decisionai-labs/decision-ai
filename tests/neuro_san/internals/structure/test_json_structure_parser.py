e4525132e8765f2f4acaeebd2da00313f4fb5ed6
Dan Fink <Daniel.Fink@cognizant.com>
2025-06-23 13:59:16 -0700

    Start a unit test for testing JsonStructureParser

---
+++ tests/neuro_san/internals/structure/test_json_structure_parser.py
@@ -0,0 +1,42 @@
+
+# Copyright (C) 2023-2025 Cognizant Digital Business, Evolutionary AI.
+# All Rights Reserved.
+# Issued under the Academic Public License.
+#
+# You can be released from the terms, and requirements of the Academic Public
+# License by purchasing a commercial license.
+# Purchase of a commercial license is mandatory for any use of the
+# neuro-san SDK Software in commercial settings.
+#
+# END COPYRIGHT
+from typing import Any
+from typing import Dict
+
+from unittest import TestCase
+
+from neuro_san.internals.structure.json_structure_parser import JsonStructureParser
+
+
+class TestJsonStructureParser(TestCase):
+    """
+    Unit tests for JsonStructureParser class.
+    """
+
+    def test_assumptions(self):
+        """
+        Can we construct?
+        """
+        parser = JsonStructureParser()
+        self.assertIsNotNone(parser)
+
+    def test_no_structure(self):
+        """
+        Tests no structure in response.
+        """
+        test: str = "This has no structure in it"
+        parser = JsonStructureParser()
+
+        structure: Dict[str, Any] = parser.parse_structure(test)
+        self.assertIsNone(structure)
+        remainder: str = parser.get_remainder()
+        self.assertIsNone(remainder)
