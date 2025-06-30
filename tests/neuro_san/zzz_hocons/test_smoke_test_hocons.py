ab0f41acd08b7c31ebe9d31d448b9d12ee099bf5
vincent.nguyen <vincent.nguyen@cognizant.com>
2025-06-30 15:29:30 -0700

    add test case llm gemini to smoke test

--- tests/neuro_san/zzz_hocons/test_smoke_test_hocons.py
+++ tests/neuro_san/zzz_hocons/test_smoke_test_hocons.py
@@ -90,7 +90,22 @@ class TestSmokeTestHocons(TestCase):
     @pytest.mark.smoke
     @pytest.mark.non_default_llm_provider
     @pytest.mark.anthropic
+    @pytest.mark.gemini
     def test_hocon_with_anthropic(self, test_name: str, test_hocon: str):
+        """
+        Test method for a single parameterized test case specified by a hocon file.
+        Arguments to this method are given by the iteration that happens as a result
+        of the magic of the @parameterized.expand annotation above.
+
+        :param test_name: The name of a single test.
+        :param test_hocon: The hocon file of a single data-driven test case.
+        """
+        # Call the guts of the dynamic test driver.
+        # This will expand the test_hocon file name from the expanded list to
+        # include the file basis implied by the __file__ and path_to_basis above.
+        self.DYNAMIC.one_test_hocon(self, test_name, test_hocon)
+    
+    def test_hocon_with_gemini(self, test_name: str, test_hocon: str):
         """
         Test method for a single parameterized test case specified by a hocon file.
         Arguments to this method are given by the iteration that happens as a result
