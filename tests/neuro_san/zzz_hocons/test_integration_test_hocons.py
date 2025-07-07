cfb91f3d0d540e4a2660d2e577d5fac536c364e3
vincent.nguyen <vincent.nguyen@cognizant.com>
2025-07-07 10:19:14 -0700

    Bumped timeout up

--- tests/neuro_san/zzz_hocons/test_integration_test_hocons.py
+++ tests/neuro_san/zzz_hocons/test_integration_test_hocons.py
@@ -37,7 +37,7 @@ class TestIntegrationTestHocons(TestCase):
 
         # List more hocon files as they become available here.
     ]))
-    @pytest.mark.timeout(30)  # 30 seconds for this test
+    @pytest.mark.timeout(90)  # 90 seconds for this test
     @pytest.mark.integration
     def test_hocon(self, test_name: str, test_hocon: str):
         """
