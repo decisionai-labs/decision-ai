ad6b065c8d30df659d707425616ed9f7ff601ebe
Dan Fink <Daniel.Fink@cognizant.com>
2025-10-02 10:34:43 -0700

    Update TestLlmFactory to be more exemplary

--- tests/neuro_san/internals/run_context/langchain/llms/test_llm_factory.py
+++ tests/neuro_san/internals/run_context/langchain/llms/test_llm_factory.py
@@ -12,15 +12,15 @@
 
 from typing import Any
 from typing import Dict
-from typing import List
 
 import json
 
-from langchain_core.callbacks.base import BaseCallbackHandler
 from langchain_core.language_models.base import BaseLanguageModel
 from langchain_openai.chat_models.base import ChatOpenAI
 
+from neuro_san.internals.run_context.langchain.llms.langchain_llm_client import LangChainLlmClient
 from neuro_san.internals.run_context.langchain.llms.langchain_llm_factory import LangChainLlmFactory
+from neuro_san.internals.run_context.langchain.llms.langchain_llm_resources import LangChainLlmResources
 
 
 class TestLlmFactory(LangChainLlmFactory):
@@ -50,14 +50,38 @@ class TestLlmFactory(LangChainLlmFactory):
                                     the model description in this class.
     """
 
-    def create_base_chat_model(self, config: Dict[str, Any],
-                               callbacks: List[BaseCallbackHandler] = None) -> BaseLanguageModel:
+    def create_base_chat_model(self, config: Dict[str, Any]) -> BaseLanguageModel:
         """
-        Create a BaseLanguageModel from the fully-specified llm config.
+        Create a LangChainLlmResources instance from the fully-specified llm config.
+
+        This method is provided for backwards compatibility.
+        Prefer create_llm_resources_with_client() instead,
+        as this allows server infrastructure to better account for outstanding
+        connections to LLM providers when connections drop.
+
         :param config: The fully specified llm config which is a product of
                     _create_full_llm_config() above.
-        :param callbacks: A list of BaseCallbackHandlers to add to the chat model.
         :return: A BaseLanguageModel (can be Chat or LLM)
+                Can raise a ValueError if the config's class or model_name value is
+                unknown to this method.
+        """
+        raise NotImplementedError
+
+    def create_llm_resources_with_client(self, config: Dict[str, Any],
+                                         llm_client: LangChainLlmClient = None) -> LangChainLlmResources:
+        """
+        Create a LangChainLlmResources instance from the fully-specified llm config.
+
+        :param config: The fully specified llm config which is a product of
+                    _create_full_llm_config() above.
+        :param llm_client: A LangChainLlmClient instance, which by default is None,
+                        implying that create_base_chat_model() needs to create its own client.
+                        Note, however that a None value can lead to connection leaks and requests
+                        that continue to run after the request connection is dropped in a server
+                        environment.
+        :return: A LangChainLlmResources instance containing
+                a BaseLanguageModel (can be Chat or LLM) and all related resources
+                necessary for managing the model run-time lifecycle.
                 Can raise a ValueError if the config's class or model_name value is
                 unknown to this method.
         """
@@ -72,38 +96,75 @@ class TestLlmFactory(LangChainLlmFactory):
         print(f"In TestLlmFactory for {json.dumps(config, sort_keys=True, indent=4)}")
         if chat_class == "test-openai":
             print("Creating test-openai")
-            llm = ChatOpenAI(
-                            model_name=model_name,
-                            temperature=config.get("temperature"),
-                            openai_api_key=self.get_value_or_env(config, "openai_api_key",
-                                                                 "OPENAI_API_KEY"),
-                            openai_api_base=self.get_value_or_env(config, "openai_api_base",
-                                                                  "OPENAI_API_BASE"),
-                            openai_organization=self.get_value_or_env(config, "openai_organization",
-                                                                      "OPENAI_ORG_ID"),
-                            openai_proxy=self.get_value_or_env(config, "openai_organization",
-                                                               "OPENAI_PROXY"),
-                            request_timeout=config.get("request_timeout"),
-                            max_retries=config.get("max_retries"),
-                            presence_penalty=config.get("presence_penalty"),
-                            frequency_penalty=config.get("frequency_penalty"),
-                            seed=config.get("seed"),
-                            logprobs=config.get("logprobs"),
-                            top_logprobs=config.get("top_logprobs"),
-                            logit_bias=config.get("logit_bias"),
-                            streaming=True,     # streaming is always on. Without it token counting will not work.
-                            n=1,                # n is always 1.  neuro-san will only ever consider one chat completion.
-                            top_p=config.get("top_p"),
-                            max_tokens=config.get("max_tokens"),    # This is always for output
-                            tiktoken_model_name=config.get("tiktoken_model_name"),
-                            stop=config.get("stop"),
 
-                            # Set stream_usage to True in order to get token counting chunks.
-                            stream_usage=True,
-                            callbacks=callbacks)
+            # See if there is an async_client to be had from the llm_client passed in
+            async_client: Any = None
+            if llm_client is not None:
+                async_openai_client = llm_client.get_client()
+                if async_openai_client is not None:
+                    # Necessary reach-in
+                    async_client = async_openai_client.chat.completions
+
+            # Now construct LLM chat model we will be using:
+            llm = ChatOpenAI(
+                async_client=async_client,
+                model_name=model_name,
+                temperature=config.get("temperature"),
+
+                # This next group of params should always be None when we have async_client
+                openai_api_key=self.get_value_or_env(config, "openai_api_key",
+                                                     "OPENAI_API_KEY", async_client),
+                openai_api_base=self.get_value_or_env(config, "openai_api_base",
+                                                      "OPENAI_API_BASE", async_client),
+                openai_organization=self.get_value_or_env(config, "openai_organization",
+                                                          "OPENAI_ORG_ID", async_client),
+                openai_proxy=self.get_value_or_env(config, "openai_organization",
+                                                   "OPENAI_PROXY", async_client),
+                request_timeout=self.get_value_or_env(config, "request_timeout", None, async_client),
+                max_retries=self.get_value_or_env(config, "max_retries", None, async_client),
+
+                presence_penalty=config.get("presence_penalty"),
+                frequency_penalty=config.get("frequency_penalty"),
+                seed=config.get("seed"),
+                logprobs=config.get("logprobs"),
+                top_logprobs=config.get("top_logprobs"),
+                logit_bias=config.get("logit_bias"),
+                streaming=True,  # streaming is always on. Without it token counting will not work.
+                n=1,  # n is always 1.  neuro-san will only ever consider one chat completion.
+                top_p=config.get("top_p"),
+                max_tokens=config.get("max_tokens"),  # This is always for output
+                tiktoken_model_name=config.get("tiktoken_model_name"),
+                stop=config.get("stop"),
+
+                # The following three parameters are for reasoning models only.
+                reasoning=config.get("reasoning"),
+                reasoning_effort=config.get("reasoning_effort"),
+                verbosity=config.get("verbosity"),
+
+                # If omitted, this defaults to the global verbose value,
+                # accessible via langchain_core.globals.get_verbose():
+                # https://github.com/langchain-ai/langchain/blob/master/libs/core/langchain_core/globals.py#L53
+                #
+                # However, accessing the global verbose value during concurrent initialization
+                # can trigger the following warning:
+                #
+                # UserWarning: Importing verbose from langchain root module is no longer supported.
+                # Please use langchain.globals.set_verbose() / langchain.globals.get_verbose() instead.
+                # old_verbose = langchain.verbose
+                #
+                # To prevent this, we explicitly set verbose=False here (which matches the default
+                # global verbose value) so that the warning is never triggered.
+                verbose=False,
+
+                # Set stream_usage to True in order to get token counting chunks.
+                stream_usage=True
+            )
+
         elif chat_class is None:
             raise ValueError(f"Class name {chat_class} for model_name {model_name} is unspecified.")
         else:
             raise ValueError(f"Class {chat_class} for model_name {model_name} is unrecognized.")
 
-        return llm
+        # Return the LlmResources with the llm_client that was passed in.
+        # That might be None, and that's OK.
+        return LangChainLlmResources(llm, llm_client=llm_client)
