a1e2080cec72a88408e204e8f0d1425d668f9bb1
Dan Fink <Daniel.Fink@cognizant.com>
2025-10-10 08:48:41 -0700

    Convert structure validator test to abstract

--- tests/neuro_san/internals/validation/test_structure_validator.py
+++ tests/neuro_san/internals/validation/test_structure_validator.py
@@ -15,66 +15,28 @@ from typing import List
 
 from unittest import TestCase
 
-from neuro_san import REGISTRIES_DIR
-from neuro_san.internals.graph.persistence.agent_network_restorer import AgentNetworkRestorer
-from neuro_san.internals.graph.registry.agent_network import AgentNetwork
+from neuro_san.internals.interfaces.agent_network_validator import AgentNetworkValidator
 from neuro_san.internals.validation.structure_network_validator import StructureNetworkValidator
 
+from tests.neuro_san.internals.validation.abstract_network_validator_test import AbstractNetworkValidatorTest
 
-class TestStructureNetworkValidator(TestCase):
+
+class TestStructureNetworkValidator(TestCase, AbstractNetworkValidatorTest):
     """
     Unit tests for StructureNetworkValidator class.
     """
 
-    @staticmethod
-    def restore(file_reference: str):
-        # Open a known good network file
-        restorer = AgentNetworkRestorer()
-        hocon_file: str = REGISTRIES_DIR.get_file_in_basis(file_reference)
-        agent_network: AgentNetwork = restorer.restore(file_reference=hocon_file)
-        config: Dict[str, Any] = agent_network.get_config()
-        return config
-
-    def test_assumptions(self):
+    def create_validator(self) -> AgentNetworkValidator:
         """
-        Can we construct?
+        Creates an instance of the validator
         """
-        validator = StructureNetworkValidator()
-        self.assertIsNotNone(validator)
-
-    def test_empty(self):
-        """
-        Tests empty network
-        """
-        validator = StructureNetworkValidator()
-
-        errors: List[str] = validator.validate(None)
-        self.assertEqual(1, len(errors))
-
-        errors: List[str] = validator.validate({})
-        self.assertEqual(1, len(errors))
-
-    def test_valid(self):
-        """
-        Tests a valid network
-        """
-        validator = StructureNetworkValidator()
-
-        # Open a known good network file
-        config: Dict[str, Any] = self.restore("hello_world.hocon")
-
-        errors: List[str] = validator.validate(config)
-
-        failure_message: str = None
-        if len(errors) > 0:
-            failure_message = errors[0]
-        self.assertEqual(0, len(errors), failure_message)
+        return StructureNetworkValidator()
 
     def test_multiple_front_men(self):
         """
         Tests a network where there is > 1 front man.
         """
-        validator = StructureNetworkValidator()
+        validator: AgentNetworkValidator = self.create_validator()
 
         # Open a known good network file
         config: Dict[str, Any] = self.restore("hello_world.hocon")
@@ -89,7 +51,7 @@ class TestStructureNetworkValidator(TestCase):
         """
         Tests a network where there is an unreachable agent.
         """
-        validator = StructureNetworkValidator()
+        validator: AgentNetworkValidator = self.create_validator()
 
         # Open a known good network file
         config: Dict[str, Any] = self.restore("esp_decision_assistant.hocon")
@@ -105,7 +67,7 @@ class TestStructureNetworkValidator(TestCase):
         """
         Tests a network where there is an unreachable agent.
         """
-        validator = StructureNetworkValidator()
+        validator: AgentNetworkValidator = self.create_validator()
 
         # Open a known good network file
         config: Dict[str, Any] = self.restore("esp_decision_assistant.hocon")
